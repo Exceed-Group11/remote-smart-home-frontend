d33d1d2bca2b4241f22eca6e98d45b1b371d0086
MyLife4What <greatzasin12@gmail.com>
2022-02-19 12:24:12 +0700

    change home path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/addform' element={<AddForm />} />
       </Routes>
