2df97c871db2e0e1e221c44857785545ad066156
MyLife4What <raweerot.p@ku.th>
2022-02-20 03:28:09 +0700

    Edit some UI and fix some bugs

--- src/components/RemoteCard.js
+++ src/components/RemoteCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { FaPowerOff } from 'react-icons/fa'
 
-const RemoteCard = ({ remote, onDelete }) => {
+const RemoteCard = ({ remote, onToggle, onDelete }) => {
   return (
     <div>
-        <div className='card'>
+        <div className='remote-card'>
             <button className='btn-remote-remove' onClick={() => onDelete(remote.remoteId)} >
                 Remove Remote
             </button>
             <h2>{remote.remoteName}</h2>
-            <button className='btn-power'>
+            <button className='btn-power' onClick={() => onToggle(remote.remoteId)}>
                 <FaPowerOff className='power'/>
             </button>
         </div>
