7a4c2e8584472f00402480f92fdd4f1f19610ad0
MyLife4What <raweerot.p@ku.th>
2022-02-20 00:13:58 +0700

    bug fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 import {  logout } from '../services/auth'
 import axios from 'axios'
 import { getSessionId } from '../utils/cookie_util'
+import { useState } from 'react'
 
 
-async function getUserInfo() {
-  let sessionId = getSessionId()
-  const response = await axios({
-      method: 'GET',
-      url: 'https://ecourse.cpe.ku.ac.th/exceed11/api/user/',
-      headers: {
-          "Authorization": `Bearer ${sessionId}`
-      }
+const NavBar = () => {
+  const [userStatus, setUserStatus] = useState(null)
+
+  async function getUserInfo() {
+    let sessionId = getSessionId()
+    const response = await axios({
+        method: 'GET',
+        url: 'https://ecourse.cpe.ku.ac.th/exceed11/api/user/',
+        headers: {
+            "Authorization": `Bearer ${sessionId}`
+        }
+    })
+  
+    if (response.status === 200) {
+      setUserStatus(true)
+    } else {
+      setUserStatus(false)
+    }      
+  }
+
+  useEffect(() => {
+    getUserInfo()
   })
 
-  if (response.status === 200) {
-    return true;
-  } else {
-    return false;
-  }      
-}
-
-
-const NavBar = () => async {
-
-
-  if ((await getUserInfo())) {
+  if (userStatus) {
       return (
       <Navbar className='navbar-orange'>
         <Container>
